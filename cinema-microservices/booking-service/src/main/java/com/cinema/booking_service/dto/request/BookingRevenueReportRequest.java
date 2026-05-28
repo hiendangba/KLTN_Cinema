1bce0366d1245649ed8b7c57bb27c86c80b74274
hiendangba <22110320@student.hcmute.edu.vn>
2026-05-29 05:58:33 +0700

    feat: add NotNull validation for PageRequest in revenue report request DTOs

--- cinema-microservices/booking-service/src/main/java/com/cinema/booking_service/dto/request/BookingRevenueReportRequest.java
+++ cinema-microservices/booking-service/src/main/java/com/cinema/booking_service/dto/request/BookingRevenueReportRequest.java
@@ -3,6 +3,7 @@ package com.cinema.booking_service.dto.request;
 import com.cinema.dto.request.DateRange;
 import com.cinema.dto.request.PageRequest;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
